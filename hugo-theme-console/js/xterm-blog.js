4a9a50cba4ce9477caaa48fac9f18687ff68670d
younjinjeong <younjinjeong@users.noreply.github.com>
2021-03-07 03:22:07 +0000

    deploy: cda8355064a769a0fa3edaa610a75cf4a3af754c

--- hugo-theme-console/js/xterm-blog.js
+++ hugo-theme-console/js/xterm-blog.js
@@ -2,7 +2,17 @@ $(function () {
   // HACK: This should be window.Terminal once upgraded to 4.0.1
   // var term = new window.Terminal.Terminal();
   var term = new Terminal();
-  term.open(document.getElementById('terminal'));
+  term.open(document.getElementById('terminal')); 
+
+  term.setOption('theme', {
+    background: 'white',
+    foreground: 'black',
+    cursor: 'green',
+    cursorStyle: 'underscore',
+    cursorBlink: true
+  }); 
+
+  term.resize(80,6);
 
   function runBlogTerminal() {
     if (term._initialized) {
@@ -10,6 +20,10 @@ $(function () {
     }
 
     term._initialized = true;
+
+
+    /* 
+     * Comment out until we have automated Cols setup 
 
     const observer = new ResizeObserver(entries => {
 
@@ -21,14 +35,15 @@ $(function () {
 
       term.resize(cols, rows); 
     })
+    */
 
+    /*
     term.prompt = () => {
       term.write('\x1B[1;3;31m\r\nblog-ctl\x1B[0m $');
     };
+    */
 
-    term.writeln('-----------------------');
-    term.writeln('|      Blog CLI      |');
-    term.writeln('-----------------------');
+    term.writeln('Blog shell - Powered by XtermJS.org');
     prompt(term);
 
     term.onData(e => {
@@ -39,22 +54,20 @@ $(function () {
           break;
         case '\u007F': // Backspace (DEL)
           // Do not delete the prompt
-          if (term._core.buffer.x > 11) {
+          if (term._core.buffer.x > 13) {  // related with length of the prompt 
             term.write('\b \b');
           }
           break;          
-        case '108 115 10': // ls command 
-          term.write(post-list);
-          break;
         default: // Print all other characters for demo
           term.write(e);
+          
       }
     });
   }
 
   function prompt(term) {
     // term.write('\r\n$ ');
-    term.write('\x1B[74;246;38m\r\nblog-ctl\x1B[0m $ ')
+    term.write('\x1B[74;246;38m\r\nblog-shell\x1B[0m $ ')
   }
   runBlogTerminal();
 });
